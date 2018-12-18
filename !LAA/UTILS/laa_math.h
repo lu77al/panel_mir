80618aa851e472ae563e370e4f0d40bd08ea098e
Alex Lushnya <lu77al@gmail.com>
2018-12-18 17:28:46 +0200

    Save before interrupting project (Small panel)

---
+++ !LAA/UTILS/laa_math.h
@@ -0,0 +1,9 @@
+#ifndef __AL_MATH_H__
+#define __AL_MATH_H__
+
+#include "stm32f7xx_hal.h"
+
+int16_t sin_koef(int16_t v, int16_t a);
+int16_t cos_koef(int16_t r, int16_t a);
+
+#endif // __AL_MATH_H__
